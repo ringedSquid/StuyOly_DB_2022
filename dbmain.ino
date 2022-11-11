6ec43d52bf567a8576b8a5677eacbfe912200cb7
Kellen Yu <80976955+IlIll101@users.noreply.github.com>
2022-11-11 18:50:45 -0500

    Update dbmain.ino

--- dbmain.ino
+++ dbmain.ino
@@ -1,36 +1,50 @@
-/*
-DB
-*/
+/* DB */
 #include <LiquidCrystal.h>
-#define RS 2
-#define E 3
-#define D4 8
-#define D5 7
-#define D6 6
-#define D7 5
-#define RED 11
-#define GRN 10
-#define BLU 9
+#include <HX711.h>
 
-LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
-float val = 0;
-float error = 0;
+/* LCD pins, 4-bit mode */
+const int RS=2,E=3,D4=8,D5=7,D6=6,D7=5;
+/* LED pins */
+const int RED=11,GRN=10,BLU=9;
+/* HX711 amplifier pins */
+const int DT=12,LD_SCK=13;
+/* HX711 adjustment settings */
+const long OFFSET=50682624;
+const long DIVIDER=1;
 
+LiquidCrystal lcd(RS,E,D4,D5,D6,D7);
+HX711 sensor;
 
 void setup() {
   pinMode(RED,OUTPUT);
-  pinMode(GRN, OUTPUT);
-  pinMode(BLU, OUTPUT);
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
+  pinMode(GRN,OUTPUT);
+  pinMode(BLU,OUTPUT);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  sensor.begin(DT,LD_SCK);
+  sensor.set_scale();
+  sensor.tare();
+  Serial.println("ZEROED")
+  Serial.begin(9600);
+  Serial.println("Starting Test");
+  delay(1000);
+  Serial.println("3");
+  delay(1000);
+  Serial.println("2");
+  delay(1000);
+  Serial.println("1");
+  if (sensor.is_ready()) {
+    long sum = 0;
+    for (int i=0; i<10; i++) {
+      sum += sensor.get_units(10);
+    }
+    Serial.print("Value: ");
+    Serial.println(sum/10.0);
+  }
+  else {
+    Serial.println("HX711 not found");
+  }
 }
 
 void loop() {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  val = analogRead(A0) * (5.0 / 1023.0) + error;
-  lcd.print(val);
-  delay(250);
-  
 }
-
